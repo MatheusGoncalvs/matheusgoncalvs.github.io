49f87b016070303fa6c7256c3e57383988ce5d1c
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2020-04-23 17:20:25 -0300

    implementado secao de skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,4 +22,27 @@ $(document).ready(function () {
             }
         }
     });
+    $('#skills-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            645: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
 });
