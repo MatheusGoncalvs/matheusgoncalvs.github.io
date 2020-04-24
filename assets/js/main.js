cc29a4d1c35f4acc6481d8e073cc87f8858fbdde
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2020-04-24 20:54:30 -0300

    implementado a secao sobre + rollagem suave js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
 
-    const metaThemeColor = document.querySelector('meta[name=theme-color]');
-    metaThemeColor.setAttribute('content', '#3AE374');
-
     $('#destaque-carousel').owlCarousel({
         loop: true,
         autoplay: true,
@@ -43,6 +40,37 @@ $(document).ready(function () {
             },
             1000: {
                 items: 2
+            }
+        }
+    });
+    /*-----------------------------------
+    * ONE PAGE SCROLLING
+    *-----------------------------------*/
+    // Select all links with hashes
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').not('[data-toggle="tab"]').on('click', function (event) {
+        // On-page links
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
             }
         }
     });
